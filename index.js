d886d3160ca6642127fab07b847f76f5c96d3e22
jejolare <jejolare@vk.com>
2022-08-08 02:01:26 +0300

    get pages amount function

--- index.js
+++ index.js
@@ -6,5 +6,22 @@ puppeteer.use(StealthPlugin());
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://kwork.ru/');
+    await page.goto('https://kwork.ru/projects?fc=41');
+    await page.waitForTimeout(3000);
+    const pagesAmount = await page.evaluate(() => 
+        parseInt([...[...document.querySelectorAll('ul')].at(-1).querySelectorAll('li')].at(-2).innerText, 10)
+    );
+    const results = [];
+    for (let index = 0; index < pagesAmount; index++) {
+        results.push(...(await processPage(page)));
+    }
 })();
+
+async function processPage(page) {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
